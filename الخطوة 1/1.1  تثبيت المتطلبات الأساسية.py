4cb07da6890b7f2ea9ab089cf7407b5e53463b49
bachir salhi <designama23@gmail.com>
2025-07-09 15:09:47 +0100

    Create 1.1  تثبيت المتطلبات الأساسية.py
     تثبيت المتطلبات الأساسية

---
+++ الخطوة 1/1.1  تثبيت المتطلبات الأساسية.py
@@ -0,0 +1,8 @@
+# تحديث pip وتثبيت الأدوات الأساسية
+!pip install --upgrade pip
+
+# تثبيت PyTorch المناسب لـ CUDA
+!pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+# تثبيت المكتبات اللازمة
+!pip install pytorch-lightning phonemizer unidecode numpy matplotlib gdown tensorboard pyyaml
